377ac15900ba5aa488af6a469a21569ecf532ee2
totoogg <goncharov.vova2014@gamil.com>
2024-07-01 22:38:39 +0300

    feat: add eslint

--- youtube-client/src/app/app.component.spec.ts
+++ youtube-client/src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'youtube-client' title`, () => {
+  it('should have the \'youtube-client\' title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('youtube-client');
