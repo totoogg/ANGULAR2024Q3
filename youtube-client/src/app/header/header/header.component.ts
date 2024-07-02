2ed3d52ed24a74cf20e3b373f06137948ea948fc
totoogg <goncharov.vova2014@gamil.com>
2024-07-02 19:16:38 +0300

    feat: add header

--- youtube-client/src/app/header/header/header.component.ts
+++ youtube-client/src/app/header/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  value = '';
 
+  user = 'Your Name';
 }
