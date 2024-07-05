403d18e94b0bc56c0cb793a669231c8231d00a99
totoogg <goncharov.vova2014@gamil.com>
2024-07-06 00:27:02 +0300

    feat: add custom button with content projection

--- youtube-client/src/app/search/search-item/search-item.component.ts
+++ youtube-client/src/app/search/search-item/search-item.component.ts
@@ -2,13 +2,14 @@ import { Component, Input } from '@angular/core';
 import { IItem } from '../search-item.model';
 import { SliceTitlePipe } from '../../pipes/slice-title.pipe';
 import { ColorLineDirective } from '../../directives/color-line.directive';
+import { CustomButtonComponent } from '../custom-button/custom-button.component';
 
 @Component({
   selector: 'app-search-item',
   standalone: true,
   templateUrl: './search-item.component.html',
   styleUrl: './search-item.component.scss',
-  imports: [SliceTitlePipe, ColorLineDirective],
+  imports: [SliceTitlePipe, ColorLineDirective, CustomButtonComponent],
 })
 export class SearchItemComponent {
   @Input() video!: IItem;
