606d5115582d6650a517bf088747a512fb1caf7a
totoogg <goncharov.vova2014@gamil.com>
2024-07-11 15:56:08 +0300

    feat: add not found page

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { SearchResultsComponent } from './youtube/components/search-results/search-results.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NotFoundComponent],
   imports: [
     BrowserModule,
     FormsModule,
