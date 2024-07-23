2e768309dae2088c49cf0dff4975605696011965
totoogg <goncharov.vova2014@gamil.com>
2024-07-24 00:13:27 +0300

    refactor: environments values moved

--- youtube-client/src/app/core/interceptor/api.interceptor.ts
+++ youtube-client/src/app/core/interceptor/api.interceptor.ts
@@ -5,15 +5,15 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { API_URL, key } from '../../environments/environments';
+import { environment } from '../../../environments/environment.development';
 
 export const apiInterceptor: HttpInterceptorFn = (
   req: HttpRequest<unknown>,
   next: HttpHandlerFn,
 ): Observable<HttpEvent<unknown>> => {
   const reqUrl = req.clone({
-    url: `${API_URL}/${req.url}`,
-    params: req.params.set('key', key),
+    url: `${environment.API_URL}/${req.url}`,
+    params: req.params.set('key', environment.key),
   });
   return next(reqUrl);
 };
